afc3dc0de938f4eb865756bdd64c481fc8e69c12
JongGyun Park <jonggyuni@gmail.com>
2019-07-31 22:22:54 +0900

    add common loading components

---
+++ components/common/Loading.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const Wrapper = styled.section`
+  width: ${({ width }) => (width ? `${width}px` : '100vw')};
+  height: ${({ height }) => (height ? `${height}px` : '100vh')};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+/* To use rotated Image in component */
+const rotate = keyframes`
+  from { transform: rotate(0deg); }
+  to { transform: rotate(360deg); }
+`;
+
+const LoadingImage = styled.img`
+  width: ${({ size }) => `${size}px`};
+  height: ${({ size }) => `${size}px`};
+
+  animation: ${rotate} 1.5s linear infinite;
+`;
+
+const Loading = ({ size, width, height }) => {
+  const imageSrc = '/static/loading.png';
+
+  return (
+    <Wrapper>
+      <LoadingImage
+        src={imageSrc}
+        width={width}
+        height={height}
+        size={size}
+        alt="loading-image"
+      />
+    </Wrapper>
+  );
+};
+
+export default Loading;
